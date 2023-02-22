e7410635257cc405c68081d75393082b8b260f8d
Kisakye-e <eng.kisakye@gmail.com>
2023-02-22 02:46:24 -0500

    The get dealer list view and url config

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -86,9 +86,14 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/8d60ccb3-aab5-46e0-8022-870551e76f2f/dealership-package/get-dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
